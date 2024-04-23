95b8e58bbcd3e08b44fbf6963381f8960b7c55e6
Igor Korzhan <igor.human@gmail.com>
2024-04-23 23:49:59 +0300

    Task_03

---
+++ Task_003/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_003
+{
+    internal class Player : IPlayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Відтворення музики");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("Пауза під час відтворення");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Зупинка відтворення");
+        }
+
+        public void Record()
+        {
+            Console.WriteLine("Запис музики");
+        }
+    }
+}
---
+++ Task_003/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+using Task_003;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = UnicodeEncoding.Unicode;
+        Player player = new Player();
+
+        Console.WriteLine("Запускаємо відтворення:");
+        player.Play();
+        player.Pause();
+        player.Stop();
+
+        Console.WriteLine("\nЗапускаємо запис:");
+        player.Record();
+        player.Pause();
+        player.Stop();
+    }
+}
